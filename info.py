25b2c42269a7d2f3baad5d6139c6edf03a4a86a0
Thomasshelby333 <116047364+Thomasshelby333@users.noreply.github.com>
2022-12-29 22:41:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,24 +12,24 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '14505719'))
-API_HASH = environ.get('API_HASH', '620f0a2aa2cd1474a4953619b3e3643d')
+API_ID = int(environ.get('API_ID', '21925188'))
+API_HASH = environ.get('API_HASH', 'f9b377f83f916d783f25d95a2f4e232e')
 BOT_TOKEN = environ.get('BOT_TOKEN', "BotToken")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/cb0cf8d856e66a8991970.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/a29cdd9800ec77f8575a3.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/cb0cf8d856e66a8991970.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/fadf76229a7c7de7a7cff.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5296610774').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001660274107').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5431249569').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001887195187').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL', '-1001701130326')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001887195187')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
@@ -44,11 +44,11 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "rxmovies")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'rxmov_files')
 # Others
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+ZPpcbtCV204yYWU1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/RolexMoviesOX')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+hvGQ4DApC39hYzVl')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vibevibevibevibevib')
 MSG_ALRT = environ.get('MSG_ALRT', '🚩 @RolexMoviesOX Best Channel In Telegram')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001660274107'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'RolexMoviesOX')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001680243578'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vibevibevibevibevib')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', 'True')), False)
 IMDB = is_enabled((environ.get('IMDB', 'true')), True)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
@@ -61,7 +61,7 @@ LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False")
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001656768078')).split()]
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001680243578')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "False")), False)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "Flase")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
@@ -78,12 +78,12 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 
      # Auto Delete For Group Message (Self Delete) #
-SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 600))
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 3600))
 SELF_DELETE = environ.get('SELF_DELETE', True)
 if SELF_DELETE == "True":
     SELF_DELETE = True
 DOWNLOAD_TEXT_NAME = "How to download "
-DOWNLOAD_TEXT_URL = "https://t.me/RolexMoviesOX/55"
+DOWNLOAD_TEXT_URL = "https://t.me/hugfbhjin"
 CAPTION_BUTTON = "Support"
-CAPTION_BUTTON_URL = "https://t.me/RolexMoviesOX"
+CAPTION_BUTTON_URL = "https://t.me/vibevibevibevibevib"
 
