51740676835a496751fc4c0a1fa2641469bfcb51
Thomasshelby333 <116047364+Thomasshelby333@users.noreply.github.com>
2022-12-29 22:33:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/narutooxox/RolexTG.git /RolexTG
+  git clone https://github.com/Thomasshelby333/Shortlinkurl.git /RolexTG
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /RolexTG
